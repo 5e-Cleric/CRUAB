99f7bf458c4274ddbcaea22bd09c694239a2213a
5e_Cleric <91662974+5e-Cleric@users.noreply.github.com>
2022-12-08 10:57:20 +0000

    404 i 500

---
+++ error404.php
@@ -0,0 +1,29 @@
+<?php
+require_once(__DIR__ . "/header.php");
+?>
+<main class="main-content rows center">
+    <article class="error">
+        <h1>La pàgina que busques no existeix</h1>
+        <div class="error-message">
+            <h2>404 - Page not found.</h2>
+            <br>
+            <h3>
+                Ei! Com has arribat aquí? <br>
+                Si creus que ets on hauries de ser, avisa'ns a <a id="email"
+                    href="mailto:comiweb@cruab.cat">comiweb@cruab.cat</a> de que la pagina no hi és.
+            </h3>
+        </div>
+    </article>
+    <!--<article>
+    <h2>
+      Aixó es una prova per veure si podem canviar el css de les pagines d'error
+    </h2>
+    <h2>Encara així, el servidor ha fallat, sorry</h2>
+  </article>-->
+    <?php
+  require_once(__DIR__ . "/boto_pujar.php");
+  ?>
+</main>
+<?php
+require_once(__DIR__ . "/footer.php");
+?>
