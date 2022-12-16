03dd0c953cc6339f982eb83eda05df7135c105aa
5e_Cleric <91662974+5e-Cleric@users.noreply.github.com>
2022-12-16 22:43:13 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,63 +1,68 @@
 <?php
 ?>
-
-
 <footer class="columns space-evenly">
     <section id="contacte">
-      <h3>Contacta amb nosaltres!</h3>
-      <adress property="email">
-        <a id="email" class="social-link" href="mailto:juntacruab@gmail.com">juntacruab@gmail.com</a>
-      </adress>
-      <adress property="adress" typeof="PostalAdress">
-        <p property="streetAdress">Plaça Cívica, 08193 Bellaterra, Barcelona</p>
-        <p>T'esperem!</p>
-      </adress>
+        <h3>Contacta amb nosaltres!</h3>
+        <adress property="email">
+            <a id="email" class="social-link" href="mailto:juntacruab@gmail.com">juntacruab@gmail.com</a>
+        </adress>
+        <adress property="adress" typeof="PostalAdress">
+            <p property="streetAdress">Plaça Cívica, 08193 Bellaterra, Barcelona</p>
+            <p>T'esperem!</p>
+        </adress>
     </section>
     <section id="enllaços-socials">
-      <h3>Ens pots trobar a...</h3>
-      <a id="instagram" class="social-link" property="instagram" name="@"
-        href="https://www.instagram.com/cruab_clubderol/">Instagram</a>
-      <a id="facebook" class="social-link" property="facebook" name="" href="https://www.facebook.com/CRUABOficial">Facebook</a>
-      <a id="twitter" class="social-link" href="https://twitter.com/cruaboficial?">Twitter</a>
+        <h3>Ens pots trobar a...</h3>
+        <a id="instagram" class="social-link" property="instagram" name="@"
+            href="https://www.instagram.com/cruab_clubderol/">Instagram</a>
+        <a id="facebook" class="social-link" property="facebook" name=""
+            href="https://www.facebook.com/CRUABOficial">Facebook</a>
+        <a id="twitter" class="social-link" href="https://twitter.com/cruaboficial?">Twitter</a>
     </section>
     <section class="socis hidden">
-      <label for="socisactius">Socis actius: </label>
-      <span id="socisactius"></span>
+        <label for="socisactius">Socis actius: </label>
+        <span id="socisactius"></span>
     </section>
     <section id="textos-legals">
-      <h3>Textos legals</h3>
-      <p>
-        <a href="/avis-legal/" target="_blank" rel="noopener noreferrer">Avís Legal</a>
-        <br>
-        <a href="/politica-de-privacitat/" target="_blank" rel="noopener noreferrer"> Política de Privacitat i
-          Cookies</a><br>
-        <!--<a href="/drets-dimatge/" target="_blank" rel="noopener noreferrer"> Drets de cessió de dades i imatges</a><br>
+        <h3>Textos legals</h3>
+        <p>
+            <a href="/avis-legal/" target="_blank" rel="noopener noreferrer">Avís Legal</a>
+            <br>
+            <a href="/politica-de-privacitat/" target="_blank" rel="noopener noreferrer"> Política de Privacitat i
+                Cookies</a><br>
+            <!--<a href="/drets-dimatge/" target="_blank" rel="noopener noreferrer"> Drets de cessió de dades i imatges</a><br>
         <a href="/estatuts-del-club-excursionista-esparreguera/" target="_blank" rel="noopener noreferrer"> Estatuts del
           Club</a><br>
         <a href="/politica-de-xarxes-socials/" target="_blank" rel="noopener noreferrer"> Política de Xarxes Socials</a>-->
-      </p>
+        </p>
     </section>
-  </footer>
 
-  <!--<script src="js/checkjunta.js"></script>-->
-  <script src="js/contingut.js"></script>
-  <script src="js/eleccions.js"></script>
-  <script src="js/gestio.js"></script>
-  <script src="js/jocsindividuals.js"></script>
-  <script src="js/jocstaula.js"></script>
-  <script src="js/lloguerjocs.js"></script>
-  <script src="js/lloguermanuals.js"></script>
-  <script src="js/manuals.js"></script>
-  <script src="js/membre.js"></script>
-  <script src="js/registre.js"></script>
-  <script src="js/validarlloguerjoc.js"></script>
-  <script src="js/validarlloguermanual.js"></script>
-  <script src="js/validarmembres.js"></script>
-
-  <script src="js/sessions.js"></script>
-  <script src="js/temesimenus.js"></script>
-  <script src="js/onload.js"></script>
-  <!--<script src="js/checkmembre.js"></script>-->
+    <button id="pujar" class="especial hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+            <path d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 
+          45.3 0L160 141.2V448c0 17.7 14.3 32 32 32s32-14.3 32-32V141.2L329.4 246.6c12.5 12.5 32.8 
+          12.5 45.3 0s12.5-32.8 0-45.3l-160-160z" />
+        </svg>
+    </button>
+</footer>
+<!--<script src="js/checkjunta.js"></script>-->
+<script src="js/contingut.js"></script>
+<script src="js/eleccions.js"></script>
+<script src="js/gestio.js"></script>
+<script src="js/jocsindividuals.js"></script>
+<script src="js/jocstaula.js"></script>
+<script src="js/lloguerjocs.js"></script>
+<script src="js/lloguermanuals.js"></script>
+<script src="js/manuals.js"></script>
+<script src="js/membre.js"></script>
+<script src="js/registre.js"></script>
+<script src="js/validarlloguerjoc.js"></script>
+<script src="js/validarlloguermanual.js"></script>
+<script src="js/validarmembres.js"></script>
+<script src="js/sessions.js"></script>
+<script src="js/temesimenus.js"></script>
+<script src="js/onload.js"></script>
+<!--<script src="js/checkmembre.js"></script>-->
 </body>
 
 </html>
