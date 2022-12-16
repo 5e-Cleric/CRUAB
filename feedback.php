c9868dc61dca586d1df46de39c76168f676fcbdd
5e_Cleric <91662974+5e-Cleric@users.noreply.github.com>
2022-12-16 22:31:56 +0100

    Create feedback.php

---
+++ feedback.php
@@ -0,0 +1,25 @@
+<?php
+require_once(__DIR__ . "/header.php");
+?>
+<main class="main-content rows center">
+    <article>
+        <h1 clas="page-title">Feedback</h1>
+        <section class="article">
+            <div class="header columns align">
+                <h2 class="titol">Envia-ns la teva opinió!</h2>
+            </div>
+            <form action="">
+            <textarea name="feedback content" id="feedback-content" cols="30" rows="10"></textarea>
+            <button class="constructiu" type="submit">Submit</button>
+            </form>
+            <br>
+            <span>DISCLAIMER: La teva opinió serà enviada a un correu brossa que llegeix un neozelandès deprimit els dimecres al matí.</span>
+
+            
+        </section>
+
+    </article>
+</main>
+<?php
+require_once(__DIR__ . "/footer.php");
+?>
