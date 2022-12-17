4f192be632478d67c72238726648b431a0e21308
Victor Losada Hernandez <losada.hernandez.victor@gmail.com>
2022-12-17 22:46:44 +0100

    fix archivos rotos

---
+++ header.php
@@ -0,0 +1,94 @@
+<?php
+require_once(__DIR__ . "/connectDB.php");
+//require_once(__DIR__."/config.php");
+?>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cruab</title>
+    <link rel="icon" type="image/x-icon" href="/img/logo/dragon.ico">
+    <link rel="stylesheet" type="text/css" href="./css/general.css">
+</head>
+
+<body>
+    <header id="capçalera" class="navigation">
+        <nav id="navbar">
+            <a class="logo enllaç-navegacio" style="max-width:100px;" href="index.php">
+                <img src="img/logo/logo.svg" alt="Logo" title="Logo" />
+            </a>
+            <a id="paginasobrenosaltres" class="enllaç-navegacio" aria-current="page" href="sobrenosaltres.php">Sobre
+                nosaltres</a>
+            <a id="paginaactualitat" class="enllaç-navegacio" aria-current="page" href="actualitat.php">Actualitat</a>
+            <a id="paginaludoteca" class="enllaç-navegacio" aria-current="page" href="ludoteca.php">Ludoteca</a>
+            <a id="paginaeleccions" class="enllaç-navegacio hidden" aria-current="page"
+                href="eleccions.php">Eleccions</a>
+
+
+
+
+            </div>
+            <div id="float-right">
+                <div class="dropdown-parent">
+                    <button id="temes" class="nav-button theme-button" type="button" data-target="theme-dropdown">
+                        <span class="material-symbols-rounded">palette</span>
+                        <span class="material-symbols-rounded">expand_more</span>
+                    </button>
+                    <dropdown id="theme-dropdown" data-collapsed="yes" class="theme-dropdown">
+                        <button id="clar" class="canvi-tema" type="button" data-toggle="theme" data-target="body"
+                            disabled>Clar</button>
+                        <button id="fosc" class="canvi-tema" type="button" data-toggle="theme" data-target="body"
+                            disabled>Fosc</button>
+                        <button id="pergami" class="canvi-tema" type="button" data-toggle="theme"
+                            data-target="body">Pergami</button>
+                        <button id="lgbt" class="canvi-tema" type="button" data-toggle="theme" data-target="body"
+                            disabled>Lgbt</button>
+                    </dropdown>
+                </div>
+                <div class="dropdown-parent">
+                    <a id="paginalogin" class="enllaç-navegacio hidden" aria-current="page" href="login.php">Iniciar
+                        sessió</a>
+
+                    <button id="user-dropdown-button" class="nav-button row center hidden">
+                        <span class="material-symbols-rounded">expand_more</span>
+                        <div class="image-holder">
+                            <img id="user-image" src="" alt="Your user image">
+                        </div>
+                    </button>
+
+
+                    <dropdown id="user-menu" data-collapsed="yes">
+                        <a href="membre.php">El meu compte</a>
+                        <a href="lloguers.php">Els meus lloguers</a>
+                        <a href="feedback.php">Feedback</a>
+                        <a id="paginalogout">Tancar sessió</a>
+                    </dropdown>
+
+                </div>
+            </div>
+        </nav>
+    </header>
+    <button class="neutre hidden" id="juntanav-button"><span
+            class="material-symbols-rounded">keyboard_double_arrow_up</span></button>
+    <nav id="juntanavbar" data-collapsed="yes">
+        <a class="" href="gestiomembres.php" data-title="Ingresos membres">
+            <div class="anti-rotate column center">
+                <span class="material-symbols-rounded">supervised_user_circle</span>
+                <span>Usuari</span>
+            </div>
+        </a>
+        <a class="" href="gestiolloguers.php" data-title="Gestió lloguers">
+            <div class="anti-rotate column center">
+                <span class="material-symbols-rounded">casino</span>
+                <span>Llog.</span>
+            </div>
+        </a>
+        <a class="" href="gestiolloguersmanuals.php" data-title="Gestió lloguers manuals">
+            <div class="anti-rotate column center">
+                <span class="material-symbols-rounded">extension</span>
+                <span>Rol</span>
+            </div>
+        </a>
+    </nav>
---
+++ ludoteca.php
@@ -0,0 +1,27 @@
+<?php
+require_once(__DIR__ . "/header.php");
+?>
+<main class="main-content ludoteca column center">
+    <article class="column center">
+        <div class="column center">
+            <h1>Ludoteca</h1>
+        </div>
+        <br>
+        <div class="escollir">
+            <a href="jocstaula.php" class="image-holder" data-title="Jocs de taula">
+                <img src="https://img.joomcdn.net/0c9f5432a199507c2667641603bb36a52df024ba_original.jpeg"
+                    alt="Jocs de taula">
+            </a>
+            <a href="manuals.php" class="image-holder" data-title="Manuals de rol">
+                <img src="https://images.ctfassets.net/swt2dsco9mfe/5j4I9MGSt44T67nDoQuEk7/0a1bd4e9b0814e1bb6df92cf793e53a1/rJGxllAtvbBhYnO.hero.jpg?q=70&w=1920"
+                    alt="Manuals de rol">
+            </a>
+        </div>
+
+
+
+    </article>
+</main>
+<?php
+require_once(__DIR__ . "/footer.php");
+?>
